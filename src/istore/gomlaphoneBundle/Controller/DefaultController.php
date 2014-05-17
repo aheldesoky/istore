e35dfc7a9d955aa116048390f10e20f6eb538863
Ahmed Eldesouky <aheldesoky@gmail.com>
2014-05-17 13:33:47 +0200

    build 1.8

--- src/istore/gomlaphoneBundle/Controller/DefaultController.php
+++ src/istore/gomlaphoneBundle/Controller/DefaultController.php
@@ -4,11 +4,13 @@ namespace istore\gomlaphoneBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use istore\gomlaphoneBundle\Entity\Customer;
 
 class DefaultController extends Controller
 {
-    public function indexAction(Request $request)
+    public function indexAction(Request $request , Customer $customer = null)
     {
+        //var_dump($customer);die;
         /*
         $factory = $this->get('security.encoder_factory');
         $user = new \istore\SecurityBundle\Entity\User();
@@ -19,12 +21,11 @@ class DefaultController extends Controller
         */
        //var_dump($_SERVER);die;
         
-        
-        
         //$user = $this->get('security.context')->getToken()->getUser();
         //var_dump($user);die;
         return $this->render('istoregomlaphoneBundle:Default:index.html.twig', array(
             'datetime'      => new \DateTime(),
+            'customer'      => $customer
         ));
     }
 }
